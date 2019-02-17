6315a760c082593aacb1cb95634245ca1a276f50
Sara <Sara@DESKTOP-OELKIJO>
2019-02-16 19:51:26 -0700

    Initial Version

---
+++ cse360/src/cse360assign2/Calculator.java
@@ -0,0 +1,72 @@
+/**
+ * @author Sara Loucks
+ * CSE 360 11369
+ * Assignment 2
+ * 
+ * cse360assign2 package contains calculator class
+ * to perform mathematical operations
+ */
+package cse360assign2;
+
+/**
+ * Calculator class keeps track of total variable
+ * and performs operations on total
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constructor sets total to 0 initially
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * returns the current total
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * adds value to total
+	 * @param value    value to add
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * subtracts value from total
+	 * @param value    value to subtract
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiplies total by a value
+	 * @param value    value to multiply by
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divides total by a value
+	 * @param value    value to divide by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * keep track of operations on total as a string
+	 * @return    string of operations performed
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
